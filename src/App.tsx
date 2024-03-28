06b6a9d0eb5fbaa339d7fbe3f544df55b2568058
NovikovIlya <novikov.i.sergeevich@gmail.com>
2024-03-29 00:02:09 +0300

    Добавил стиля

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./styles.css";
 import React, { useState } from "react";
+import { Button, Modal } from 'antd';
 import {
   BarChart,
   Bar,
@@ -9,8 +10,10 @@ import {
   Tooltip,
   Legend,
 } from "recharts";
+import styles from './App.module.css'
 
 export default function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const [monValue, setMonValue] = useState(100);
   const data = [
     {
@@ -20,47 +23,54 @@ export default function App() {
     },
     {
       name: "tue",
-
       pv: 1398,
       amt: 2210,
     },
     {
       name: "wed",
-
       pv: 9800,
       amt: 2290,
     },
     {
       name: "thu",
-
       pv: 3908,
       amt: 2000,
     },
     {
       name: "fri",
-
       pv: 4800,
       amt: 2181,
     },
     {
       name: "sat",
-
       pv: 3800,
       amt: 2500,
     },
     {
       name: "sun",
-
       pv: 4300,
       amt: 2100,
     },
   ];
-  const handle = (e) => {
+  const handle = (e: any) => {
     setMonValue(e.target.value);
   };
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
   return (
-    <>
-      <input onChange={handle} />
+    <div className={styles.container}>
+      <div className={styles.inp}>
+        <input onChange={handle} />
+      </div>
       <BarChart
         width={500}
         height={300}
@@ -76,9 +86,18 @@ export default function App() {
         <XAxis dataKey="name" />
         <YAxis />
         <Tooltip />
-        <Legend />
-        <Bar dataKey="pv" stackId="a" fill="#8884d8" />
+        {/* <Legend /> */}
+        <Bar dataKey="pv" stackId="a" fill="#EC755D" />
       </BarChart>
-    </>
+      <Modal
+        title="Basic Modal"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <p>Some contents...</p>
+      </Modal>
+      <button onClick={showModal}>sss</button>
+    </div>
   );
 }
